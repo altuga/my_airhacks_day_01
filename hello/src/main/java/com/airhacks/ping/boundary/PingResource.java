0ffa199a0aa76e75648d0c0bf4e30885cf890b69
AltugAltintas <altuga@gmail.com>
2019-05-21 01:23:43 +0900

    interceptors explained done

--- hello/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ hello/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -1,5 +1,6 @@
 package com.airhacks.ping.boundary;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -9,10 +10,15 @@ import javax.ws.rs.Path;
  */
 @Path("ping")
 public class PingResource {
+    
+    @Inject
+    Pingy pingy ;
 
     @GET
     public String ping() {
-        return "Enjoy Java EE 8!";
+        //return "Enjoy Java EE 8!";
+        
+        return pingy.pingMe() + " with " + pingy.getClass().getName();
     }
 
 }
